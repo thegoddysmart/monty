45468eee173ccfd4363565284acfe60ec8c4d55a
dealdrain <dealdrain@yahoo.com>
2023-10-19 15:13:28 -0700

    ops 1

--- op_codes_1.c
+++ op_codes_1.c
@@ -37,7 +37,7 @@ void pop_op(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", line_num);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		free_all_data();
 		exit(EXIT_FAILURE);
 	}
