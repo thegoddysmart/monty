88ea68615e11a568527b64faac075ed41cfb13af
Godfred <goddysmart2468@gmail.com>
2023-10-19 12:35:00 -0700

    pops the top item from the stack

--- op_codes_1.c
+++ op_codes_1.c
@@ -24,3 +24,22 @@ void push_op(stack_t **stack, unsigned int line_number)
 	val = _atoi(glob.arg[1]);
 	add_stack_node(stack, val);
 }
+
+
+/**
+ * pop_op - this pops the top item from the stack
+ * @stack: The stack itself
+ * @line_number: this is where the command is
+ * Return: Always void
+ */
+
+void pop_op(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_num);
+		free_all_data();
+		exit(EXIT_FAILURE);
+	}
+	/* yet to implement the delete node operation */
+}
