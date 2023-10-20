5bb25760b88d1906650186aa7ffd86c8ed58b87e
Godfred <goddysmart2468@gmail.com>
2023-10-19 19:37:02 -0700

    reads all lines from file

--- execute.c
+++ execute.c
@@ -38,3 +38,45 @@ void execute_opcode(instruction_t opcodes_arr[], char **all_lines)
 	}
 	free_all_data();
 }
+
+
+/**
+ * reader - this reads all lines from a file
+ * @argv: filename
+ * Return: An array of all lines
+ */
+
+char **reader(char *argv)
+{
+	FILE *fileptr;
+	size_t bufsize = 1;
+	ssize_t char_read = 0;
+	char **lines_array = NULL, *line_buffer = NULL, *temp;
+	int line_count = 0, total_lines = 0;
+
+	fileptr = fopen(argv, "r");
+	if (fileptr == NULL)
+		handle_file_err(argv);
+	while (getline(&line_buffer, &bufsize, fileptr) != -1)
+		total_lines++;
+	fclose(fileptr);
+	lines_array = malloc(sizeof(char *) * (total_lines + 1));
+	if (lines_array == NULL)
+		malloc_error();
+
+	fileptr = fopen(argv, "r");
+	while ((char_read = getline(&line_buffer, &bufsize, fileptr)) != -1)
+	{
+		temp = strtok(line_buffer, "\n");
+		if (temp == NULL)
+			continue;
+
+		lines_array[line_count] = dup_str(temp);
+		line_count++;
+	}
+	lines_array[line_count] = NULL;
+	fclose(fileptr);
+	if (char_read == -1)
+		free(line_buffer);
+	return (lines_array);
+}
