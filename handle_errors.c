a06b71b317581ef13467f356795831bb4207e11f
Godfred <goddysmart2468@gmail.com>
2023-10-19 18:54:37 -0700

    this handles not found error

--- handle_errors.c
+++ handle_errors.c
@@ -10,3 +10,19 @@ void malloc_error(void)
 	fprintf(stderr, "Error: malloc failed\n");
 	exit(EXIT_FAILURE);
 }
+
+
+/**
+ * handle_not_found - this handles not found error
+ * @all_lines: lines array to free
+ * @index: this is the line number
+ * @cmds: command not found
+ */
+
+void handle_not_found(char **all_lines, int index, char *cmds)
+{
+	printf("L%d: unknown instruction %s\n", index, cmds);
+	free_str_array(all_lines);
+	free_stack_nodes(&(glob.stack));
+	exit(EXIT_FAILURE);
+}
